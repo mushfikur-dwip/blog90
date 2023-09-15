be77e3e2e39f464bb0e0c88053a78afd41d946fb
Mushfikur Rahman <90860586+mushfikur-dwip@users.noreply.github.com>
2023-09-16 02:15:13 +0600

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Mushfikur Rahman",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "Web developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    email: "mushfikurrahmandwip@gmail.com",
+    linkedin: "mushfikur_dwip",
+    github: "mushfikur-dwip",
+    instagram: "mushfikur_dwip",
   },
   projects: [
     {
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Mushfikur Rahman",
+    description: "welcome!",
   },
 
   // CONFIG configration (required)
