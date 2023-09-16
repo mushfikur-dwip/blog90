b700966977304f2692382e0afd2dd6114ee2d020
Mushfikur Rahman <90860586+mushfikur-dwip@users.noreply.github.com>
2023-09-16 08:48:52 +0600

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `KajKhuji.com.bd`,
+      href: "https://kajkhuji.com.bd",
     },
   ],
   // blog setting (required)
